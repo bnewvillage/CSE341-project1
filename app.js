486b04ff80e0e3b77af0c3db8e9e4da4274de054
bnewvillage <blainenewvillage@gmail.com>
2025-03-12 11:36:52 +0400

    initial full commit with get all and get single functions

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
+
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(PORT , ()=>{
-    console.log(`Running on port: ${PORT}`);
-})
+mongodb.initDb((err) =>{
+    if(err){
+        console.log(err);
+    } else {
+        app.listen(PORT , ()=>{
+            console.log(`Database is listening and node running on port: ${PORT}`);
+        })
+    }
+});
